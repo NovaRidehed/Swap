13fcb1c419c8ffd8577bd27e14c2470a11bdc8a2
Nova Ridehed <nova.ridehed@skola.sala.se>
2024-03-21 14:04:26 +0100

    Add project files.

---
+++ Swap/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap
+            int[] swapTestData = { 8, 4, 6, 9 };
+            Console.WriteLine("Demonstration av Swap:");
+            Console.WriteLine("Före: " + string.Join(",", swapTestData));
+            Swap(1, 3, swapTestData); // ska fungera med olika index!
+            Console.WriteLine("Efter: " + string.Join(",", swapTestData));
+        }
+        public static void Swap(int index1, int index2, int[] numbers)
+        {
+            // TODO
+            int previntarray = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = previntarray;
+        }
+    }
+}
